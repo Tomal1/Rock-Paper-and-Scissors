bc87a0463de746f2cbd3d1c00d802465ed257fc5
Minhazul Islam <minhazulislam@hotmail.com>
2022-11-11 20:44:24 +0000

    intro page leads into second page that gerates 3 buttons R P & S

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,24 @@
-console.log("hellow");
+
+let startBtn = document.querySelector("#startBtn");
+let intro = document.querySelector("#intro");
+let secondScreen = document.querySelector("#secondScreen");
+let buttons = document.querySelector(".buttons");
+
+startBtn.addEventListener("click", function(){
+    intro.style.visibility = "hidden";
+    second();
+})
+
+function second(){
+    let rock = document.createElement("button");
+    rock.innerHTML = "ROCK";
+    buttons.appendChild(rock);
+
+    let paper = document.createElement("button");
+    paper.innerHTML = "PAPER";
+    buttons.appendChild(paper);
+
+    let scissor = document.createElement("button");
+    scissor.innerHTML = "SCISSOR";
+    buttons.appendChild(scissor);
+}
